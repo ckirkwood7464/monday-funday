274edaf891ad1c5d2ec4679ac33149512f2e2bc9
Connor Kirkwood <ckirkwood7464@gmail.com>
2019-09-30 21:42:29 -0400

    Added Cars

---
+++ objects-in-array.js
@@ -0,0 +1,8 @@
+var cars = [
+    { make: 'Jeep', model: 'Wrangler', year: 2013, color: 'black', style: 'Unlimited Sport'},
+    { make: 'Jeep', model: 'Grand Cherokee', year: 2011, color: 'red', style: 'Loredo'},
+    { make: 'Jeep', model: 'Liberty', year: 2009, color: 'maroon', style: 'Sport'},
+    { make: 'Jeep', model: 'Wrangler', year: 2013, color: 'blue', style: 'Sport'}
+]
+
+console.log(cars)
